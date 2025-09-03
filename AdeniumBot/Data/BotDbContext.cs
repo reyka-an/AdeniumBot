31e78df5cc169399d0f3934e5e0dd964d50f50d9
Reyka <blueorange477@gmail.com>
2025-09-04 00:04:28 +0300

    fix db

--- AdeniumBot/Data/BotDbContext.cs
+++ AdeniumBot/Data/BotDbContext.cs
@@ -1,46 +1,42 @@
-using Adenium.Models;
 using Microsoft.EntityFrameworkCore;
+using Adenium.Models;
 
 namespace Adenium.Data
 {
     public class BotDbContext : DbContext
     {
         private readonly string _conn;
-        public BotDbContext(string connectionString) => _conn = connectionString;
+        
+        public BotDbContext(string connectionString)
+        {
+            _conn = connectionString;
+        }
+        protected BotDbContext() { _conn = ""; }
 
         public DbSet<PlayerProfile> PlayerProfiles => Set<PlayerProfile>();
         public DbSet<FavoriteLink> FavoriteLinks => Set<FavoriteLink>();
         public DbSet<BlacklistLink> BlacklistLinks => Set<BlacklistLink>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql(_conn);
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseNpgsql(_conn);
+        }
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
             var pp = mb.Entity<PlayerProfile>();
             pp.HasIndex(p => p.DiscordUserId).IsUnique();
-            
+
             var fav = mb.Entity<FavoriteLink>();
             fav.HasKey(x => new { x.OwnerId, x.TargetId });
-            fav.HasOne(x => x.Owner)
-                .WithMany(p => p.Favorites)
-                .HasForeignKey(x => x.OwnerId)
-                .OnDelete(DeleteBehavior.Restrict);
-            fav.HasOne(x => x.Target)
-                .WithMany(p => p.FavoritedBy)
-                .HasForeignKey(x => x.TargetId)
-                .OnDelete(DeleteBehavior.Restrict);
-            
+            fav.HasOne(x => x.Owner).WithMany(p => p.Favorites).HasForeignKey(x => x.OwnerId).OnDelete(DeleteBehavior.Restrict);
+            fav.HasOne(x => x.Target).WithMany(p => p.FavoritedBy).HasForeignKey(x => x.TargetId).OnDelete(DeleteBehavior.Restrict);
+
             var bl = mb.Entity<BlacklistLink>();
             bl.HasKey(x => new { x.OwnerId, x.TargetId });
-            bl.HasOne(x => x.Owner)
-                .WithMany(p => p.Blacklist)
-                .HasForeignKey(x => x.OwnerId)
-                .OnDelete(DeleteBehavior.Restrict);
-            bl.HasOne(x => x.Target)
-                .WithMany(p => p.BlacklistedBy)
-                .HasForeignKey(x => x.TargetId)
-                .OnDelete(DeleteBehavior.Restrict);
+            bl.HasOne(x => x.Owner).WithMany(p => p.Blacklist).HasForeignKey(x => x.OwnerId).OnDelete(DeleteBehavior.Restrict);
+            bl.HasOne(x => x.Target).WithMany(p => p.BlacklistedBy).HasForeignKey(x => x.TargetId).OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
