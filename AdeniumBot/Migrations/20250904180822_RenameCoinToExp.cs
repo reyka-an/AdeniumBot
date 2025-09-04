10eed43ca194cf36f23e9dad736db936bb21effa
Reyka <blueorange477@gmail.com>
2025-09-04 21:09:25 +0300

    fix

---
+++ AdeniumBot/Migrations/20250904180822_RenameCoinToExp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdeniumBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCoinToExp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
